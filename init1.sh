9cefa9e18847c79d6221e139b1b3ca0ade1a7873
Jens Weggemann <jensweh@gmail.com>
2015-01-26 15:41:06 +0100

    Added Vundle initialisation.

--- init1.sh
+++ init1.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p .vim/unversioned/bundle
-mkdir -p .vim/unversioned/tmp
+mkdir -p ~/.vim/unversioned/bundle
+mkdir -p ~/.vim/unversioned/tmp
 
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/unversioned/bundle/Vundle.vim
